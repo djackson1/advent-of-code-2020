34c589a5a76e223335db5df631c56c853241e414
Daniel Jackson <d95jackson@gmail.com>
2020-12-04 00:03:53 +0000

    part b

--- src/day01/main.js
+++ src/day01/main.js
@@ -1,9 +1,6 @@
 const { getInputs } = require('../../utils/files')
 
-function find2020andMultiply(inputs){
-  const orderedHigh = inputs.sort((a, b) => b - a)
-  const orderedLow = orderedHigh.reverse()
-
+function findSumToAndMultiply(orderedLow, orderedHigh, desiredSum = 2020){
   for(let i = 0; i < orderedLow.length; i++){
     const low = orderedLow[i]
 
@@ -13,25 +10,57 @@ function find2020andMultiply(inputs){
       const sum = low + high
 
       // if the sum matches
-      if(sum === 2020) {
+      if(sum === desiredSum) {
         return low * high
       }
 
       // if the sum of low plus the next highest number we have is less than
       // our desired sum, then we can skip to a new low number
-      if(sum < 2020) continue;
+      if(sum < desiredSum) continue;
     }
   }
 
   return -1
 }
 
-function a () {
+function getOrderedInputs () {
   const inputs = getInputs(1).map(Number)
+  const orderedHigh = inputs.sort((a, b) => b - a)
+  const orderedLow = orderedHigh.reverse()
 
-  const value = find2020andMultiply(inputs)
+  return { orderedLow, orderedHigh }
+}
+
+function a () {
+  const { orderedLow, orderedHigh } = getOrderedInputs()
+  const value = findSumToAndMultiply(orderedLow, orderedHigh)
 
   console.log(`day 01 a => ${value}`)
 }
 
+function find2020In3Inputs (orderedLow, orderedHigh) {
+  for(let i = 0; i < orderedLow.length; i++){
+    const low = orderedLow[i]
+    const newSum = 2020 - low
+
+    const newLowArr = orderedLow.slice(i + 1)
+    
+    const result = findSumToAndMultiply(newLowArr, orderedHigh, newSum)
+
+    if(result > -1) {
+      return result * low
+    }
+  }
+
+  return -1
+}
+
+
+function b () {
+  const { orderedLow, orderedHigh } = getOrderedInputs()
+  const value = find2020In3Inputs(orderedLow, orderedHigh)
+  console.log(`day 01 b => ${value}`)
+}
+
 a()
+b()
