13bcf9b428ed956651eb8198d496a556045cbb12
Daniel Jackson <danieljackson@quillcontent.com>
2020-12-04 10:32:45 +0000

    part b

--- src/day03/main.js
+++ src/day03/main.js
@@ -1,32 +1,46 @@
 const { getInputs } = require('../../utils/files')
 const inputs = getInputs(3)
+const width = inputs[0].length
+const height = inputs.length
 
 const tree = '#'
-// slope [R=3, D=1]
 
-const a = () => {
-  const width = inputs[0].length
-  const height = inputs.length
-
+function countTreesAtSlope(dx, dy) {
   let count = 0
   const position = { x: 0, y: 0 }
 
   for (let i = 0; i < height - 1; i++) {
-    position.x += 3
-    position.y += 1
+    position.x += dx
+    position.y += dy
 
-    const item = inputs[position.y][position.x % width]
+    if (inputs[position.y]) {
+      const item = inputs[position.y][position.x % width]
 
-    if (item === tree) {
-      count += 1
+      if (item === tree) {
+        count += 1
+      }
     }
   }
+
+  return count
+}
+
+// slope = [3,1]
+const a = () => {
+  const count = countTreesAtSlope(3, 1)
 
   console.log(`a = ${count}`)
 }
 
 const b = () => {
-  console.log(`b = ${'?'}`)
+  const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]]
+
+  const treeCount = slopes.reduce((acc, [dx, dy]) => {
+    const count = countTreesAtSlope(dx, dy)
+    return acc * count
+  }, 1)
+
+  console.log(`b = ${treeCount}`)
 }
 
 a()
