2b42c3f9f91b78d59ad6fcec6e765f28db99c9f1
Daniel Jackson <d95jackson@gmail.com>
2020-12-10 08:45:28 +0000

    part a

--- src/day10/main.js
+++ src/day10/main.js
@@ -1,14 +1,15 @@
 const { getInputs } = require('../../utils/files')
-const inputs = getInputs(10)
+const inputs = [0, ...getInputs(10, { fn: Number })].sort((i,j) => i - j)
+console.log("🚀 ~ file: main.js ~ line 3 ~ inputs", inputs)
 
-const arr = [0, 
-  28,  33,  18,  42,  31,  14,  46,  20,  48,  47,  24,  23,  49,  45,  19,  38,  39,  11,  1,  32,  25,  35,  8,  17,  7,  9,  4,  2,  34,  10,  3
-].sort((i,j) => i - j)
-console.log("🚀 ~ file: main.js ~ line 5 ~ arr", arr)
+// const arr = [0, 
+//   28,  33,  18,  42,  31,  14,  46,  20,  48,  47,  24,  23,  49,  45,  19,  38,  39,  11,  1,  32,  25,  35,  8,  17,  7,  9,  4,  2,  34,  10,  3
+// ].sort((i,j) => i - j)
+// console.log("🚀 ~ file: main.js ~ line 5 ~ arr", arr)
 
 const a = () => {
-  const arr2 = arr.map((a, id) => {
-    const item2 = arr[id + 1]
+  const arr2 = inputs.map((a, id) => {
+    const item2 = inputs[id + 1]
 
     if(isNaN(item2)) return 3
 
