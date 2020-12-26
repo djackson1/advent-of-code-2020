795f3a834f5b2bacb7d356a051329841b6112a77
Daniel Jackson <d95jackson@gmail.com>
2020-12-26 19:00:10 +0000

    runner refactor

--- utils/runner.ts
+++ utils/runner.ts
@@ -10,17 +10,15 @@ const PART_BOTH = 'both';
     return console.error('No day string => "npm run start 1 a"')
   }
   
-  if (!part) {
-    return console.error('No part to solve string => "npm run start 1 a"')
-  }
-  
   const daySolutions = require(`../src/day${day}/main.ts`)
+
+  const runBoth = part === PART_BOTH || typeof part === 'undefined'
   
-  if (part !== PART_BOTH && !daySolutions[part]) {
+  if (!runBoth && !daySolutions[part]) {
     return console.error(`Can't run part "${part}" for day "${day}"`)
   }
   
-  if (part === PART_BOTH) {
+  if (runBoth) {
     daySolutions.a()
     daySolutions.b()
   } else if (part === 'a' || part === 'b') {
