f7d75057e029e83329941c67549e807e69932116
Daniel Jackson <d95jackson@gmail.com>
2021-01-08 09:05:54 +0000

    parsing file

--- src/day19/main.spec.ts
+++ src/day19/main.spec.ts
@@ -14,14 +14,14 @@ describe('day 19', () => {
   })
 
   it('should extract file details', function () {
-    expect(this.rules).to.deep.equal([
-      '0: 4 1 5',
-      '1: 2 3 | 3 2',
-      '2: 4 4 | 5 5',
-      '3: 4 5 | 5 4',
-      '4: "a"',
-      '5: "b"'
-    ])
+    expect(this.rules).to.deep.equal({
+     0: [4, 1, 5],
+     1: [[2, 3], [3, 2]],
+     2: [[4, 4], [5, 5]],
+     3: [[4, 5], [5, 4]],
+     4: "a",
+     5: "b",
+    })
     expect(this.messages).to.deep.equal(['ababbb', 'bababa', 'abbbab', 'aaabbb', 'aaaabbb'])
   })
 
